8756b0d71eea0f20a0204a348c20acd3803ef74f
Fowji <fowji@gmail.com>
2025-07-27 19:11:09 +0530

    First Time Project Config

---
+++ src/test/java/com/facebook/FacebookLogin.java
@@ -0,0 +1,24 @@
+package com.facebook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FacebookLogin {
+public static void main(String[] args) {
+	
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	
+	WebElement txtUsername = driver.findElement(By.id("email"));
+	txtUsername.sendKeys("velmurugasiromrbranch@gmail.com");
+	WebElement txtPassword = driver.findElement(By.id("pass"));
+	txtPassword.sendKeys("Greens@123");
+	Actions actions=new Actions(driver);
+	actions.keyDown(Keys.ENTER).perform();
+
+		}
+}
